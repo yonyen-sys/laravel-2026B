f9ee98140e88b6d959e9e639da84d7b661f60e52
yen yon <yonyen99@gmail.com>
2026-06-18 13:59:15 +0700

    REST API AUTH

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
-            ProductSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
@@ -9,13 +10,31 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('categories', CategoryController::class)->names([
-    'index'   => 'api.categories.index',
-    'store'   => 'api.categories.store',
-    'show'    => 'api.categories.show',
-    'update'  => 'api.categories.update',
-    'destroy' => 'api.categories.destroy',
-]);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    //  protect route (private)
+    Route::post('/categories', [CategoryController::class, 'store'])
+        ->name('api.categories.store');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])
+        ->name('api.categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
+        ->name('api.categories.destroy');
+
+    // 
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+});
+
+// public route
+Route::get('/categories', [CategoryController::class, 'index'])
+    ->name('api.categories.index');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])
+    ->name('api.categories.show');
+
+
 Route::apiResource('products', ProductController::class)->names([
     'index'   => 'api.products.index',
     'store'   => 'api.products.store',
